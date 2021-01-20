a2f0946d30b2f3c6e7299e0bb9dbb3bcf90b9d70
cxb1988 <283817727@qq.com>
2021-01-20 19:27:44 +0800

    视频文件名生成函数

--- utils/Utils.go
+++ utils/Utils.go
@@ -10,6 +10,6 @@ import (
 //视频文件名生成函数
 func GetVideoName(uid string) string {
 	h := md5.New()
-	h.Write([]byte(uid + strconv.FormatUint(time.Now().UnixNano(), 10)))
+	h.Write([]byte(uid + strconv.FormatInt(time.Now().UnixNano(), 10)))
 	return hex.EncodeToString(h.Sum(nil))
 }
